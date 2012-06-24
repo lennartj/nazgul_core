b3440e9f040f4597519f82c86b72d0ddf633e076
Lennart J?relid <lj@jguru.se>
2012-06-25 00:01:05 +0200

    Initial add. Started with algorithms.

---
+++ core/algorithms/algorithms-api/src/main/java/se/jguru/nazgul/core/algorithms/api/predicate/Aggregator.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) jGuru Europe AB.
+ * All rights reserved.
+ */
+
+package se.jguru.nazgul.core.algorithms.api.predicate;
+
+/**
+ * Generic type aggregator.
+ *
+ * @author <a href="mailto:lj@jguru.se">Lennart J&ouml;relid</a>, jGuru Europe AB
+ */
+public interface Aggregator<T, R> {
+
+    /**
+     * Performs aggregation of the candidate and the current (aggregate) result.
+     *
+     * @param candidate The candidate to be reduced / aggregated into the current value.
+     * @param current   The current aggregate value. Note that this value is null on the first call to aggregate.
+     * @return The result of the aggregate operation between the current result and the provided candidate.
+     */
+    R aggregate(R current, T candidate);
+}
