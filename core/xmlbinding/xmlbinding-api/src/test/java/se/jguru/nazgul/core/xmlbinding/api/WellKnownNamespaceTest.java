0505f8d95bfe08f8a8680ebe53867a7cb984c35a
Lennart J?relid <lj@jguru.se>
2012-07-15 19:17:11 +0200

    Added experimental XmlBinding API.

---
+++ core/xmlbinding/xmlbinding-api/src/test/java/se/jguru/nazgul/core/xmlbinding/api/WellKnownNamespaceTest.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) jGuru Europe AB.
+ * All rights reserved.
+ */
+package se.jguru.nazgul.core.xmlbinding.api;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+/**
+ * @author <a href="mailto:lj@jguru.se">Lennart J&ouml;relid</a>, jGuru Europe AB
+ */
+public class WellKnownNamespaceTest {
+
+    // Shared state
+    private WellKnownNamespace[] allNamespaces = WellKnownNamespace.values();
+
+
+    @Test
+    public void validateNamespaceDataIsNeitherNullNorEmpty() {
+
+        // Assert
+        for(WellKnownNamespace current : allNamespaces) {
+
+            // Validate that the namespaceURL is not null or empty
+            Assert.assertNotNull(current.getNameSpaceUrl());
+            Assert.assertFalse("".equals(current.getNameSpaceUrl()));
+
+            // Validate that the prefix is not null or empty
+            Assert.assertNotNull(current.getXsdPrefix());
+            Assert.assertFalse("".equals(current.getXsdPrefix()));
+        }
+    }
+}
