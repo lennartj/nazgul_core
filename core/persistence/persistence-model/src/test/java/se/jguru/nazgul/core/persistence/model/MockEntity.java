eb38b0db373f4db8b4e43d13d688b9f53e94de96
Lennart Jörelid <lennartj@users.noreply.github.com>
2012-08-16 01:02:44 +0200

    Added test coverage to persistence model.

---
+++ core/persistence/persistence-model/src/test/java/se/jguru/nazgul/core/persistence/model/MockEntity.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) jGuru Europe AB.
+ * All rights reserved.
+ */
+
+package se.jguru.nazgul.core.persistence.model;
+
+import se.jguru.nazgul.core.xmlbinding.api.XmlBinder;
+import se.jguru.nazgul.tools.validation.api.exception.InternalStateValidationException;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlTransient;
+import javax.xml.bind.annotation.XmlType;
+import java.util.ArrayList;
+
+/**
+ * @author <a href="mailto:lj@jguru.se">Lennart J&ouml;relid</a>, jGuru Europe AB
+ */
+@Entity
+@XmlType(namespace = XmlBinder.CORE_NAMESPACE, propOrder = {"name", "value"})
+@XmlAccessorType(XmlAccessType.FIELD)
+@Access(value = AccessType.FIELD)
+public class MockEntity extends NazgulEntity {
+
+    // Internal state
+    @XmlElement(nillable = false, required = true)
+    private String name;
+
+    @XmlAttribute(required = true)
+    @Column(nullable = false)
+    private Long value;
+
+    @XmlTransient
+    public ArrayList<String> callTrace = new ArrayList<String>();
+
+    public MockEntity() {
+    }
+
+    public MockEntity(String name, Long value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Long getValue() {
+        return value;
+    }
+
+    /**
+     * Override this method to perform validation of the entity internal state of this Validatable.
+     * <strong>Note!</strong> The first call within the validateEntityState method should be
+     * {@code super.validateEntityState()}.
+     *
+     * @throws se.jguru.nazgul.tools.validation.api.exception.InternalStateValidationException
+     *          if the state of this Validatable was in an incorrect
+     *          state (i.e. invalid).
+     */
+    @Override
+    protected void validateEntityState() throws InternalStateValidationException {
+
+        callTrace.add("validateEntityState [" + getName() + ", " + getValue() + "]");
+
+        InternalStateValidationException.create()
+                .notNullOrEmpty(name, "name")
+                .notNull(value, "value")
+                .endExpressionAndValidate();
+    }
+}
--- core/persistence/persistence-model/src/test/java/se/jguru/nazgul/core/persistence/model/NazgulEntityTest.java
+++ core/persistence/persistence-model/src/test/java/se/jguru/nazgul/core/persistence/model/NazgulEntityTest.java
@@ -4,7 +4,10 @@
  */
 package se.jguru.nazgul.core.persistence.model;
 
+import junit.framework.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 /**
  * @author <a href="mailto:lj@jguru.se">Lennart J&ouml;relid</a>, jGuru Europe AB
@@ -12,13 +15,54 @@ import org.junit.Test;
 public class NazgulEntityTest {
 
     @Test
-    public void validateIdentityAndEquality() {
+    public void validateCreation() {
 
         // Assemble
-        final NazgulEntity
+        final String name = "foobar";
+        final Long value = 42L;
+        final MockEntity mockEntity = new MockEntity(name, value);
+
+        // Act
+        // ... do nothing...
+
+        // Assert
+        final ArrayList<String> callTrace = mockEntity.callTrace;
+        Assert.assertTrue("callTrace size: " + callTrace.size(), callTrace.size() > 0);
+    }
+
+    @Test
+    public void validateEqualityAndIdentity() {
+
+        // Assemble
+        final String name = "foobar";
+        final Long value = 42L;
+        final MockEntity mockEntity = new MockEntity(name, value);
+        final MockEntity mockEntity2 = new MockEntity(name, value);
+
+        // Act
+        final boolean equalityByComparison = mockEntity.equals(mockEntity2);
+        final boolean equalityByNull = mockEntity.equals(null);
+        final boolean hashCodeEqual = mockEntity.hashCode() == mockEntity2.hashCode();
+
+        // Assert
+        Assert.assertTrue(equalityByComparison);
+        Assert.assertFalse(equalityByNull);
+        Assert.assertTrue(hashCodeEqual);
+    }
+
+    @Test
+    public void validateCloningAndCopying() throws Exception {
+
+        // Assemble
+        final String name = "foobar";
+        final Long value = 42L;
+        final MockEntity mockEntity = new MockEntity(name, value);
 
         // Act
 
         // Assert
+        Assert.assertEquals(mockEntity, mockEntity.copy());
+        Assert.assertEquals(mockEntity, mockEntity.clone());
+        Assert.assertNotSame(mockEntity, mockEntity.copy());
     }
 }
