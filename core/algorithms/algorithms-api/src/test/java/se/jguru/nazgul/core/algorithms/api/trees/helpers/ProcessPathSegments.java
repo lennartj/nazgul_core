8fd1338f626918cdbb8abf2325f0253a2631b879
Lennart J?relid <lj@jguru.se>
2012-09-25 07:31:44 +0200

    Added testcases. Moved some collectionAPI classes.

--- core/algorithms/algorithms-api/src/test/java/se/jguru/nazgul/core/algorithms/api/trees/helpers/ProcessPathSegments.java
+++ core/algorithms/algorithms-api/src/test/java/se/jguru/nazgul/core/algorithms/api/trees/helpers/ProcessPathSegments.java
@@ -7,13 +7,10 @@ package se.jguru.nazgul.core.algorithms.api.trees.helpers;
 /**
  * @author <a href="mailto:lj@jguru.se">Lennart J&ouml;relid</a>, jGuru Europe AB
  */
-public enum ProcessPath {
-
+public enum ProcessPathSegments {
+    ROOT,
     PRODUCT,
-
     SYSTEM,
-
     CLUSTER,
-
     NODE
 }
