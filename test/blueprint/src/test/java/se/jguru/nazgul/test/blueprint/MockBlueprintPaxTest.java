5f3ef828c786ff19640254ea2974c762f367d6a1
Lennart Jörelid <lennartj@users.noreply.github.com>
2013-02-24 07:49:41 +0100

    Started transition from PojoSR to Pax in blueprint testing.

---
+++ test/blueprint/src/test/java/se/jguru/nazgul/test/blueprint/MockBlueprintPaxTest.java
@@ -0,0 +1,63 @@
+package se.jguru.nazgul.test.blueprint;
+
+import junit.framework.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.CoreOptions;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerMethod;
+import org.osgi.framework.BundleContext;
+
+import javax.inject.Inject;
+
+/**
+ * @author <a href="mailto:lj@jguru.se">Lennart J&ouml;relid</a>, jGuru Europe AB
+ */
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerMethod.class)
+public class MockBlueprintPaxTest {
+
+    // Shared state
+
+    @Inject
+    private BundleContext context;
+
+    @Configuration
+    public Option[] config() {
+
+        return CoreOptions.options(
+
+                // Add the relevant maven bundles.
+                CoreOptions.mavenBundle(
+                        "se.jguru.nazgul.test.bundles.hello.impl.blueprint",
+                        "nazgul-test-hello-impl-blueprint",
+                        "1.0.0"),
+
+                CoreOptions.mavenBundle(
+                        "se.jguru.nazgul.test.bundles.hello.api",
+                        "nazgul-test-hello-api",
+                        "1.0.0"),
+
+                // Add
+                // CoreOptions.bundle("http://www.example.com/repository/foo-1.2.3.jar"),
+
+                // Add JUnit bundles.
+                CoreOptions.junitBundles()
+        );
+    }
+
+    @Test
+    public void validateBlueprint() {
+
+        // Assemble
+        Assert.assertNotNull("Injected BundleContext is null", context);
+
+        /*
+        assertNotNull(helloService);
+        assertEquals("Hello Pax!", helloService.getMessage());
+        */
+    }
+}
