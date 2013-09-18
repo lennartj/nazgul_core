2b09600e01125bbb990728deaddc541199d39c3f
Lennart J?relid <lj@jguru.se>
2013-09-18 13:09:34 +0200

    Added the in-memory cache implementation.

--- core/cache/cache-impl-inmemory/src/main/java/se/jguru/nazgul/core/cache/impl/inmemory/NamedSequenceThreadFactory.java
+++ core/cache/cache-impl-inmemory/src/main/java/se/jguru/nazgul/core/cache/impl/inmemory/NamedSequenceThreadFactory.java
@@ -1,3 +1,24 @@
+/*
+ * #%L
+ * Nazgul Project: nazgul-core-cache-impl-inmemory
+ * %%
+ * Copyright (C) 2010 - 2013 jGuru Europe AB
+ * %%
+ * Licensed under the jGuru Europe AB license (the "License"), based
+ * on Apache License, Version 2.0; you may not use this file except
+ * in compliance with the License.
+ * 
+ * You may obtain a copy of the License at
+ * 
+ *       http://www.jguru.se/licenses/jguruCorporateSourceLicense-2.0.txt
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 package se.jguru.nazgul.core.cache.impl.inmemory;
 
 import org.apache.commons.lang3.Validate;
@@ -13,19 +34,25 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class NamedSequenceThreadFactory implements ThreadFactory {
 
     // Internal state
-    private static final AtomicInteger counter = new AtomicInteger(0);
+    private final AtomicInteger sequenceCounter = new AtomicInteger(0);
     private String threadNamePrefix;
     private int maxSequenceNumber;
     private final Object lock = new Object();
 
+    /**
+     * Convenience constructor using {@code Integer.MAX_VALUE} for the maximum sequence number.
+     *
+     * @param threadNamePrefix A string prepended to the name of any Thread created by this NamedSequenceThreadFactory.
+     */
     public NamedSequenceThreadFactory(final String threadNamePrefix) {
         this(threadNamePrefix, Integer.MAX_VALUE);
     }
 
     /**
+     * Compound constructor creating a new NamedSequenceThreadFactory from the supplied parameters.
      *
-     * @param threadNamePrefix
-     * @param maxSequenceNumber
+     * @param threadNamePrefix  A string prepended to the name of any Thread created by this NamedSequenceThreadFactory.
+     * @param maxSequenceNumber The maximum sequence number for this ThreadFactory.
      */
     public NamedSequenceThreadFactory(final String threadNamePrefix,
                                       final int maxSequenceNumber) {
@@ -39,23 +66,25 @@ public class NamedSequenceThreadFactory implements ThreadFactory {
     }
 
     /**
-     * Constructs a new {@code Thread}.  Implementations may also initialize
-     * priority, name, daemon status, {@code ThreadGroup}, etc.
+     * Constructs a new {@code Thread}, using a default threadname on the form
+     * {@code threadNamePrefix-nn}, where {@code nn} is the sequence number of the
+     * retrieved Thread.
      *
-     * @param r a runnable to be executed by new thread instance
+     * @param r a runnable to be executed by new thread instance.
      * @return constructed thread, or {@code null} if the request to
      *         create a thread is rejected
      */
     @Override
+    @SuppressWarnings("all")
     public Thread newThread(final Runnable r) {
 
         synchronized (lock) {
-            if(counter.get() == maxSequenceNumber) {
-                counter.set(0);
+            if (sequenceCounter.get() == maxSequenceNumber) {
+                sequenceCounter.set(0);
             }
         }
 
         // All done.
-        return new Thread(r, threadNamePrefix + "-" + counter.getAndIncrement());
+        return new Thread(r, threadNamePrefix + "-" + sequenceCounter.getAndIncrement());
     }
 }
---
+++ core/cache/cache-impl-inmemory/src/test/java/se/jguru/nazgul/core/cache/impl/inmemory/NamedSequenceThreadFactoryTest.java
@@ -0,0 +1,86 @@
+/*
+ * #%L
+ * Nazgul Project: nazgul-core-cache-impl-inmemory
+ * %%
+ * Copyright (C) 2010 - 2013 jGuru Europe AB
+ * %%
+ * Licensed under the jGuru Europe AB license (the "License"), based
+ * on Apache License, Version 2.0; you may not use this file except
+ * in compliance with the License.
+ * 
+ * You may obtain a copy of the License at
+ * 
+ *       http://www.jguru.se/licenses/jguruCorporateSourceLicense-2.0.txt
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+/*
+ * Copyright (c) jGuru Europe AB
+ * All rights reserved.
+ */
+package se.jguru.nazgul.core.cache.impl.inmemory;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author <a href="mailto:lj@jguru.se">Lennart J&ouml;relid</a>, jGuru Europe AB
+ */
+public class NamedSequenceThreadFactoryTest {
+
+    @Test
+    public void validateThreadNameGeneration() {
+
+        // Assemble
+        final NamedSequenceThreadFactory unitUnderTest = new NamedSequenceThreadFactory("foo");
+        final Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                // Do nothing.
+            }
+        };
+
+        // Act
+        final Thread result0 = unitUnderTest.newThread(runnable);
+        final Thread result1 = unitUnderTest.newThread(runnable);
+
+        // Assert
+        Assert.assertNotNull(result0);
+        Assert.assertNotNull(result1);
+
+        Assert.assertEquals("foo-0", result0.getName());
+        Assert.assertEquals("foo-1", result1.getName());
+    }
+
+    @Test
+    public void validateThreadSequenceRollover() {
+
+        // Assemble
+        final NamedSequenceThreadFactory unitUnderTest = new NamedSequenceThreadFactory("bar", 2);
+        final Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                // Do nothing.
+            }
+        };
+
+        // Act
+        final Thread result0 = unitUnderTest.newThread(runnable);
+        final Thread result1 = unitUnderTest.newThread(runnable);
+        final Thread result2 = unitUnderTest.newThread(runnable);
+
+        // Assert
+        Assert.assertNotNull(result0);
+        Assert.assertNotNull(result1);
+        Assert.assertNotNull(result2);
+
+        Assert.assertEquals("bar-0", result0.getName());
+        Assert.assertEquals("bar-1", result1.getName());
+        Assert.assertEquals("bar-0", result2.getName());
+    }
+}
