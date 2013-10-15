2c1a3623a0c6dad8358292c33f54035e36832d9f
Lennart J?relid <lj@jguru.se>
2013-10-15 07:54:30 +0200

    Added EnumMapPath using simpler mechanics.

--- core/algorithms/tree/tree-model/src/main/java/se/jguru/nazgul/core/algorithms/tree/model/SemanticPath.java
+++ core/algorithms/tree/tree-model/src/main/java/se/jguru/nazgul/core/algorithms/tree/model/SemanticPath.java
@@ -28,12 +28,12 @@ import java.io.Serializable;
  * An extension to Path where all segments have a semantic meaning, defined
  * by the values of an Enum. This implies that all levels of the path correspond
  * to some concept, and that the SemanticPath in essence works as a Map relating
- * Enum values to KeyType values.
+ * Enum values to SegmentType values.
  *
  * @author <a href="mailto:lj@jguru.se">Lennart J&ouml;relid</a>, jGuru Europe AB
  */
-public interface SemanticPath<E extends Enum<E>, KeyType extends Serializable & Comparable<KeyType>>
-        extends Path<KeyType> {
+public interface SemanticPath<E extends Enum<E>, SegmentType extends Serializable & Comparable<SegmentType>>
+        extends Path<SegmentType> {
 
     /**
      * @return The maximum number of segments in this Path.
@@ -41,12 +41,12 @@ public interface SemanticPath<E extends Enum<E>, KeyType extends Serializable & 
     int getMaxSize();
 
     /**
-     * Retrieves the KeyType segment corresponding to the Enum value providing
+     * Retrieves the SegmentType segment corresponding to the Enum value providing
      * the semantic definition of this SemanticPath.
      *
      * @param semanticDefinition The semantic definition within this SemanticPath whose value should be retrieved.
-     * @return The segment corresponding to the provided semantic definition, or {@code null} if no segment KeyType is
-     *         defined for the provided semanticDefinition level within this SemanticPath.
+     * @return The segment corresponding to the provided semantic definition, or {@code null} if no segment SegmentType
+     *         is defined for the provided semanticDefinition level within this SemanticPath.
      */
-    KeyType get(E semanticDefinition);
+    SegmentType get(E semanticDefinition);
 }
