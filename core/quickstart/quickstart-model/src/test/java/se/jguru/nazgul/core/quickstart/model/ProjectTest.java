9dfd4bd1ffb59e8f388e233eb241ea6961fcccba
Lennart J?relid <lj@jguru.se>
2014-06-29 10:54:53 +0200

    Added marshalling test for DependencyData.

--- core/quickstart/quickstart-model/src/test/java/se/jguru/nazgul/core/quickstart/model/ProjectTest.java
+++ core/quickstart/quickstart-model/src/test/java/se/jguru/nazgul/core/quickstart/model/ProjectTest.java
@@ -1,6 +1,28 @@
+/*
+ * #%L
+ * Nazgul Project: nazgul-core-quickstart-model
+ * %%
+ * Copyright (C) 2010 - 2014 jGuru Europe AB
+ * %%
+ * Licensed under the jGuru Europe AB license (the "License"), based
+ * on Apache License, Version 2.0; you may not use this file except
+ * in compliance with the License.
+ * 
+ * You may obtain a copy of the License at
+ * 
+ *       http://www.jguru.se/licenses/jguruCorporateSourceLicense-2.0.txt
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 package se.jguru.nazgul.core.quickstart.model;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import se.jguru.nazgul.core.xmlbinding.spi.jaxb.JaxbXmlBinder;
 import se.jguru.nazgul.test.xmlbinding.AbstractStandardizedTimezoneTest;
@@ -17,13 +39,13 @@ public class ProjectTest extends AbstractStandardizedTimezoneTest {
     /**
      * {@inheritDoc}
      */
-    @Override
-    protected void setupSharedState() {
+    @Before
+    public void setupSharedState() {
         binder = new JaxbXmlBinder();
     }
 
     @Test
-    public void validateMarshalling() {
+    public void validateMarshalling() throws Exception {
 
         // Assemble
         final SimpleArtifact reactorParent = new SimpleArtifact(
@@ -35,8 +57,20 @@ public class ProjectTest extends AbstractStandardizedTimezoneTest {
 
         // Act
         final String result = binder.marshal(unitUnderTest);
+        // System.out.println("Got: " + result);
 
         // Assert
-        XmlTestUtils.compareXmlIgnoringWhitespace(expected, result);
+        Assert.assertTrue(XmlTestUtils.compareXmlIgnoringWhitespace(expected, result).identical());
+    }
+
+    @Test
+    public void validateUnmarshalling() throws Exception {
+
+        // Assemble
+        final String data = XmlTestUtils.readFully("testdata/project.xml");
+
+        // Act
+
+        // Assert
     }
 }
